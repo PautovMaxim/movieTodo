71d945986b731068f39b12474c09fb0f5ced5ca7
Maxim <131585699+PautovMaxim@users.noreply.github.com>
2023-11-12 14:52:23 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,8 @@ const movieListNode = document.querySelector('.movie__list');
 /*Создаём массив, в который будут помещаться наши фильмы*/
 let movies = [];
 
+init();
+
 /*Объявляем событие по форме(если нажать enter/button, то происходит отправка)*/
 movieFormNode.addEventListener('submit', addMovie);
 movieListNode.addEventListener('click', deleteMovie);
@@ -16,81 +18,61 @@ movieListNode.addEventListener('click', doneMovie);
 
 /*Функции*/
 function addMovie(event) {
-    event.preventDefault(); // отменяет стандартное поведение формы
-
-    if (movieInputNode.value === '') { //если в инпуте пусто, выходим из функции
-        return;
-    }
+    event.preventDefault();//обнуляем поведение формы
     
-    const movieName = movieInputNode.value; // берём значение с инпута и заносим в переменную
+    const movieName = movieInputNode.value;//объявляем переменную со значением с инпута
 
-    const newMovie = { //создаём объект(фильм)
-        done: false, // выполнен/не вынополнен
-        movie: movieName, //Название фильма
-        id: Math.random(), // индивидуальный идентификатор для фильма
+    const newMovie = {
+        id: Math.random(),//задаём рандомный айдишник
+        movie: movieName,//свойству присваваем значение переменной movieName
+        checked: false,//добавляем состояние выполненно/нет
     }
 
-    movies.push(newMovie);//заносим в массив наш объект(фильм)
+    movies.unshift(newMovie);
 
-    //формируем css класс
-    //если newMovie.done = true, то добавляем класс 'inactive', если false - оставляем как есть
-
-    //добавление элемента в список HTML
-    let movieHTML = '';
-    movies.forEach(function(newMovie, index) {
-        movieHTML += `
-        <li id="${newMovie.id}" class="movie__item">
-            <input
-                id="movie_${index}"
-                class="movie__checkbox"
-                type="checkbox"
-            />
-            <label class="movie__label" for="movie_${index}"
-                >${newMovie.movie}</label
-            >
-            <button class="movie__remove" data-action="delete"></button>
-        </li>
-        `;
-        
-        movieListNode.innerHTML = movieHTML;
-    });
+    renderMovie(movies);
 
     saveToLocalStorage();
 
-    movieInputNode.value = ''; //очищаем инпут
-    movieInputNode.focus(); //фокусируем на инпуте обратно
+    movieInputNode.value = '';
+    movieInputNode.focus();
 }
 
 function deleteMovie(event) {
-    if (event.target.dataset.action !== 'delete') {
+    //если кликл произошел не по элементу с атрибутом, выходим из функции 
+    if(event.target.dataset.action !== 'delete') {
         return;
     }
-
+    //в противном случае выполняем код:
     const parentNode = event.target.closest('.movie__item'); //находим элемент по классу
-    
-    const id = Number(parentNode.id); //находим id 
+    const id = Number(parentNode.dataset.id); //находим айди элемента по которому было совершенно действие
 
-    movies = movies.filter(function(newMovie) { //изменяем массив, с помощью фильрации
+    //изменяем массив с помощью фильрации
+    //если айди равен newMovie.id, значит элемент нужно удалить, поэтому возвращаем false и элемент не проходит в новый массив
+    movies = movies.filter(function(newMovie) {
         if (id === newMovie.id) {
             return false;
         } else {
             return true;
         }
-    }) // те айдишники newMovie.id, которые не равны parentNode.id попадают в массив, остальные нет
+    })
 
     saveToLocalStorage();
     
+    //удаляем элемент из разметки
     parentNode.remove();
+
 }
 
 function doneMovie(event) {
-    if (!event.target.classList.contains('movie__label')) {
+    //если клик был совершён не по label, выходим из функции
+    if (!event.target.classList.contains('movie__label')){
         return;
     }
 
     const parentNode = event.target.closest('.movie__item');
-    
-    const id = Number(parentNode.id);
+    const id = Number(parentNode.dataset.id);
+    // const checkboxNode = document.getElementById(id);
 
     const newMovie = movies.find(function(newMovie) {
         if (id === newMovie.id) {
@@ -98,37 +80,75 @@ function doneMovie(event) {
         }
     })
 
-    newMovie.done = !newMovie.done
+    
+    newMovie.checked = !newMovie.checked;
 
     saveToLocalStorage();
 
-    parentNode.classList.toggle('inactive')
+    parentNode.classList.toggle('inactive');
 }
 
+//Создание карточки
+function renderMovie(movies) {
+    movieListNode.innerHTML = '';
+
+    movies.forEach(newMovie => {
+        //создаём переменные, в которых будут хранится HTML элементы
+        const movieItem = document.createElement('li');
+        const movieLabel = document.createElement('label');    
+        const movieCheckbox = document.createElement('input');    
+        const movieRemoveBtn = document.createElement('button');
+
+        //добавляем атрибут 'data-id=newMovie.id'
+        movieItem.dataset.id = newMovie.id;
+        //добавляем атрибут 'data-action=delete'
+        movieRemoveBtn.dataset.action = 'delete';
+        
+        //генерируем эти элементы на странице
+        movieListNode.appendChild(movieItem);
+        movieItem.appendChild(movieCheckbox);
+        movieItem.appendChild(movieLabel);
+        movieItem.appendChild(movieRemoveBtn);
+
+        //добавляем классы к элементам
+        movieItem.className = 'movie__item';
+        movieCheckbox.className = 'movie__checkbox';
+        movieLabel.className = 'movie__label';
+        movieRemoveBtn.className = 'movie__remove';
+
+        //добавляем атрибуты к элементам
+        movieCheckbox.setAttribute('type', 'checkbox');
+        movieCheckbox.setAttribute('id', newMovie.id);
+        movieLabel.setAttribute('for', newMovie.id);
+
+        //название фильма отображаем в label
+        movieLabel.innerText = newMovie.movie;
+
+        checkStateCard (movieItem, movieCheckbox, newMovie);
+    });
+}
+
+// Проверка состояния карточки
+function checkStateCard (movieItem, movieCheckbox, newMovie) {
+    if (newMovie.checked) {
+        movieItem.classList.add('inactive');
+        movieCheckbox.setAttribute('checked', '');
+    } else {
+        movieItem.classList.remove('inactive');
+        movieCheckbox.setAttribute('unchecked', '');
+    };
+}
+
+//Сохранение данных в локальное хранилище
 function saveToLocalStorage() {
     localStorage.setItem('movies', JSON.stringify(movies));
 }
 
-if (localStorage.getItem('movies')) {
+function init() {
+    if (localStorage.getItem('movies')) {
     movies = JSON.parse(localStorage.getItem('movies'));
 
-    let movieHTML = '';
-    movies.forEach(function(newMovie, index) {
-        movieHTML += `
-        <li id="${newMovie.id}" class="movie__item">
-            <input
-                id="movie_${index}"
-                class="movie__checkbox"
-                type="checkbox"
-            />
-            <label class="movie__label" for="movie_${index}"
-                >${newMovie.movie}</label
-            >
-            <button class="movie__remove" data-action="delete"></button>
-        </li>
-        `;
-        
-        movieListNode.innerHTML = movieHTML;
-    });
+    renderMovie(movies);
+    }
 }
 
